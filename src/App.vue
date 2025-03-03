7a89b934d3c121a6b1a379d7ddf5753ffa81ce89
PurityKomen <jepkorirpurity8@gmail.com>
2025-03-03 08:22:36 +0300

    initialize state using ref

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
+ import { ref } from 'vue';
 
+ const name = ref('John Doe')
+ const status = ref('active')
+ const tasks = ref(['Task One','Task Two','Task Three'])
 </script>
 
 <template>
