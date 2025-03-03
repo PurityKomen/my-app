933c47306d53ad3b657cf59196e6b16812c4cc9f
PurityKomen <jepkorirpurity8@gmail.com>
2025-03-03 10:13:43 +0300

    added button to toggle different status

--- src/App.vue
+++ src/App.vue
@@ -4,37 +4,42 @@ import { ref } from "vue";
 const name = ref("John Doe");
 const status = ref("active");
 const tasks = ref(["Task One", "Task Two", "Task Three"]);
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
 </script>
 
 <template>
-  <h1>Hello {{ name }}</h1>
+  <div class="container">
+    <h1>Hello {{ name }}</h1>
 
-  <div class="status">
-    <p v-if="status === 'active'">User is active</p>
-    <p v-else-if="status === 'pending'">User is pending</p>
-    <p v-else>User is inactive</p>
-  </div>
+    <div class="status">
+      <p v-if="status === 'active'">User is active</p>
+      <p v-else-if="status === 'pending'">User is pending</p>
+      <p v-else>User is inactive</p>
+    </div>
 
-  <div class="task">
-    <h1>Tasks</h1>
-    <ul>
-      <li v-for="task in tasks" :key="task">{{ task }}</li>
-    </ul>
+    <div class="task">
+      <h1>Tasks</h1>
+      <ul>
+        <li v-for="task in tasks" :key="task">{{ task }}</li>
+      </ul>
+    </div>
+
+    <button @click="toggleStatus">Change Status</button>
   </div>
 </template>
 
 <style scoped>
-* {
-  margin-top: 20px;
-}
-
-.status{
- display: flex;
- margin: 20px;
-}
-
-.task{
+.task {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 20px;
 }
 </style>
