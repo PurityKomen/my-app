ba8470dd6da32446384e51f026f150b948fb0ecc
PurityKomen <jepkorirpurity8@gmail.com>
2025-03-03 09:34:48 +0300

    added v-if and v-for directives

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
- import { ref } from 'vue';
+import { ref } from "vue";
 
- const name = ref('John Doe')
- const status = ref('active')
- const tasks = ref(['Task One','Task Two','Task Three'])
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
 </script>
 
 <template>
-  <h1>Hello</h1>
-</template>
+  <h1>Hello {{ name }}</h1>
 
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h1>Tasks</h1>
+  <ul>
+    <li v-for="task in tasks" :key="task"> {{ task }}</li>
+  </ul>
+</template>
