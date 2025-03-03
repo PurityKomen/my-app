fce90a02156432778f72198a4c334c64c475c509
PurityKomen <jepkorirpurity8@gmail.com>
2025-03-03 09:56:23 +0300

    added styling

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,32 @@ const tasks = ref(["Task One", "Task Two", "Task Three"]);
 <template>
   <h1>Hello {{ name }}</h1>
 
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
+  <div class="status">
+    <p v-if="status === 'active'">User is active</p>
+    <p v-else-if="status === 'pending'">User is pending</p>
+    <p v-else>User is inactive</p>
+  </div>
 
-  <h1>Tasks</h1>
-  <ul>
-    <li v-for="task in tasks" :key="task"> {{ task }}</li>
-  </ul>
+  <div class="task">
+    <h1>Tasks</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </div>
 </template>
+
+<style scoped>
+* {
+  margin-top: 20px;
+}
+
+.status{
+ display: flex;
+ margin: 20px;
+}
+
+.task{
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
